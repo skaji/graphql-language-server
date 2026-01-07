7f144d7aaab7d87a193b027eab8d00d5722a14bc
Shoichi Kaji <skaji@cpan.org>
2026-01-08 04:47:55 +0900

    fix lint.sh

--- lint.sh
+++ lint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 prettier --check .
 golangci-lint run ./...
 
 GOPLS_OUT=$(git ls-files '*.go' | xargs gopls check -severity hint)
-echo "$GOPLS_OUT"
 if [[ -n $GOPLS_OUT ]]; then
+  echo "$GOPLS_OUT"
   exit 1
 fi
